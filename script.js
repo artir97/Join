a0dbf29087e9e2c9af01c8361de9b0fbe6b27e98
artir97 <85441675+artir97@users.noreply.github.com>
2023-11-17 07:45:26 +0100

    add top part of the navigation bar for desktop

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function init(){
+async function init(){
+    await includeHTML();
     setTimeout(loadimg, 800);
 }
+
+
 function loadimg(){
     let container = document.getElementById('logo-container');
     let img = document.getElementById('logo-img');
@@ -9,4 +12,18 @@ function loadimg(){
     img.style.left = '5%';
     container.style.backgroundColor = "white"; 
     document.getElementById('login-container').style.display = 'flex';
+}
+
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html"); // "includes/header.html"
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
 }
