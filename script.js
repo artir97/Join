a23a1c895ad9054d12e4cd8a7d4f592458d00e48
Khroxx <134767175+Khroxx@users.noreply.github.com>
2023-11-21 13:49:35 +0100

    contacts progress

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ let contacts = [
 async function init(){
     await includeHTML();
     setTimeout(loadimg, 800);
+    contactsListRender();
 }
 
 
@@ -72,6 +73,49 @@ async function includeHTML() {
         }
     }
 }
-function addNewContact(){
 
+
+function contactsListRender(){
+    let contactsList = document.getElementById('contacts');
+    contacts.sort((a, b) => a.name.localeCompare(b.name));
+    for (let i = 0; i < contacts.length; i++) {
+        const contact = contacts[i];
+        contactsList.innerHTML += contactList(contact);
+    }
+}
+
+function contactList(contact){
+    let firstLetter = contact['name'].charAt(0).toUpperCase();
+    if (contactList.lastFirstLetter === undefined || contactList.lastFirstLetter !== firstLetter) {
+        contactList.lastFirstLetter = firstLetter;
+        return `
+            <div class="firstLetter">${firstLetter}</div>
+            ${contactInfo(contact, firstLetter)}
+        `;
+    } else {
+        return `
+            ${contactInfo(contact, firstLetter)}
+        `;
+    }
+}
+
+function contactInfo(contact){
+    let uppercaseLetters = (str) => {return str.split('').filter(char => /[A-Z]/.test(char));};
+    const uppercaseLetter = uppercaseLetters(contact['name']).join('');
+    return `
+        <div class="spacer">
+        <svg xmlns="http://www.w3.org/2000/svg" width="353" height="2" viewBox="0 0 353 2" fill="none">
+        <path d="M0.5 1H352.5" stroke="#D1D1D1" stroke-linecap="round"/>
+        </svg>
+        </div>
+        <div class="contactInfo">
+        <div class"profileBadge">
+            ${uppercaseLetter}
+                <svg xmlns="http://www.w3.org/2000/svg" width="43" height="42" viewBox="0 0 43 42" fill="none">
+                    T<circle cx="21.5" cy="21" r="20" fill="#FF7A00" stroke="white" stroke-width="2"/>
+                </svg>
+                </div>
+                <h2>${contact['name']} <p>${contact['email']}</p></h2>
+            </div>
+    `
 }
