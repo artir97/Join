b79e6606013d7afd38af55d9ba199da04e42d761
artir97 <85441675+artir97@users.noreply.github.com>
2023-11-23 14:30:30 +0100

    open and close add task form in board

---
+++ js/board.js
@@ -0,0 +1,8 @@
+function closeAddTaskForm(){
+    document.getElementById('slide-form-add-task').style.display = 'none';
+}
+
+function openAddTaskForm(){
+    document.getElementById('slide-form-add-task').style.display = 'block';
+
+}
