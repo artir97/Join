224921018f5440d68c1165ca23f23d419a42f611
artir97 <85441675+artir97@users.noreply.github.com>
2023-12-04 10:34:01 +0100

    mini 'fix' to get access to the board through the guest button

---
+++ login.js
@@ -0,0 +1,7 @@
+console.log('login.js');
+function login(){
+    console.log('ich bin in der login() funktion');
+    
+    let email = document.getElementById('login');
+    let password = document.getElementById('password');
+}
